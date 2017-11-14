e9c31bb9ca2a9ad74233a865845f3bd183c1a2b4
asoler91 <32654790+Asoler19@users.noreply.github.com>
2017-11-13 16:34:30 -0800

    2nd commit

--- js/main.js
+++ js/main.js
@@ -7,38 +7,44 @@ const colors = [
     'rgb(255, 0, 0)',
     'rgb(0, 255, 0)',
     'rgb(0, 127, 255)',
-    'rgb(255, 255, 127)'
+    'rgb(138, 67, 67)',
+    'rgb(55, 80, 35)',
+    'rgb(50, 42, 56)',
+    'rgb(187, 175, 197)',
   ]
 
-  // here is the class
   class Squares {
     constructor (boxes, colors) {
-      // When instantiate, the constructor is executed and assign an event listener for each box
       this.addClickHandler(boxes, colors);
     }
 
-    changeNumber (i, boxes) {
-      console.log("Runnig function changeNumber");
-      // i is the index of the clicked box on the box array
-      //changed the inner text of each box 
+    changeNumber (i, boxes, ) {
       for(var k = i; k >= 0; k--) {
         const n = parseInt(boxes[k].innerText) + 1;
-        boxes[k].innerText = (n > 9)? 1 : n;
+        boxes[k].innerText = (n >= 12)? 1 : n;
       }
+      return k
     }
 
     changeColor (i, boxes, colors) {
-      console.log("Runnig function changeColor");
       for(var k = i; k >= 0; k--) {
         let style = window.getComputedStyle(boxes[k]);
         let index = colors.indexOf(style.backgroundColor);
         boxes[k].style.backgroundColor = (index > 7)? colors[0] : colors[index + 1];
         console.log(style.backgroundColor, index);
       }
+      return k
+
+    }
+    increment (i, boxes, colors){
+      for(var k = i; k >= 0; k--) {
+        return k
+      }
+     
     }
 
+
     addClickHandler (boxes, colors) {
-      // here I assigned the eventListener to each box
       boxes.forEach((box, i) => {
         box.addEventListener('click', evt => {
           this.changeNumber(i, boxes);
@@ -47,10 +53,11 @@ const colors = [
       })
     }
   }
+
   const boxes = document.querySelectorAll('.col');
   const squares = new Squares(boxes, colors);
 
-  // here I initializated the boxes 
+
   boxes.forEach((el, i) => {
     el.innerText = i + 1;
     el.style.backgroundColor = colors[i];
